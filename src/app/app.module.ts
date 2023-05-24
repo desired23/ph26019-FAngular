e248a320367ac8b2f57ee56c862da761a9bac632
desired23 <huydang2312003@gmail.com>
2023-05-24 18:11:45 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { LayoutAdminComponent } from './components/layouts/layout-admin/layout-admin.component';
+import { LayoutClientComponent } from './components/layouts/layout-client/layout-client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundPageComponent,
+    DashboardComponent,
+    ProductsComponent,
+    LayoutAdminComponent,
+    LayoutClientComponent
   ],
   imports: [
     BrowserModule,
